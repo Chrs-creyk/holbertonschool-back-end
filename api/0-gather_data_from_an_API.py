b99144c7d824311c81ac30894ea6d874ec7355ec
Chrs-creyk <chrisacount117@gmail.com>
2023-04-26 07:00:13 -0500

    upgrading 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-'''returns information about his/her TODO list progress.'''
-import pandas as pd
+"""Module"""
+
 import requests
 import sys
-'''data analysis'''
+
+
 if __name__ == '__main__':
-    reponse = requests.get('https://jsonplaceholder.typicode.com/todos/')
-    '''data analysis'''
-    data = pd.read_json(reponse.text)
-    total_number_of_task = len(data[data['userId'] == int(sys.argv[1])])
-    completed = len(data[(data['userId'] == int(
-        sys.argv[1])) & (data['completed'] == True)])
+    user_id = sys.argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    todos_url = user_url + "/todos/"
 
-    reponse2 = requests.get('https://jsonplaceholder.typicode.com/users/')
-    data2 = pd.read_json(reponse2.text)
-    employee_name = data2[data2['id'] == int(sys.argv[1])]['name'].values[0]
-    task_title = list(data[(data['userId'] == int(sys.argv[1])) & (
-        data['completed'] == True)]['title'])
+    user_info = requests.request('GET', user_url).json()
+    todos_info = requests.request('GET', todos_url).json()
 
-    print("Employee {} is done with tasks({}/{}):".format(employee_name,
-          completed, total_number_of_task))
+    emp_name = user_info["name"]
+    task_completed = list(filter(lambda obj:
+                                 (obj["completed"] is True), todos_info))
+    number_tasks = len(task_completed)
+    total_tasks = len(todos_info)
 
-    for i in task_title:
-        print("\t {}".format(i))
+    print("Employee {} is done with tasks({}/{}):".
+          format(emp_name, number_tasks, total_tasks))
+
+    [print("\t " + task["title"]) for task in task_completed]
